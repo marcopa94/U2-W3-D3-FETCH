6b9539e39439fe5d3d7ca93063c89ca146a3fbfa
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-02-28 18:26:31 +0100

    aggiustamentiii

--- index.js
+++ index.js
@@ -15,26 +15,33 @@ fetch("https://striveschool-api.herokuapp.com/books")
     books.forEach((book) => {
       /*    console.log(book.title); */
       const btn1 = document.createElement("button");
+      const btn2 = document.createElement("button");
       btn1.classList.add("btn", "btn-danger", "mt-4");
+      btn2.classList.add("btn", "btn-success", "mt-4");
       btn1.innerText = "cancel";
+      btn2.innerText = "Aggiungi carrello";
       btn1.addEventListener("click", () => {
         row.removeChild(card);
       });
 
       const card = document.createElement("div");
-      card.classList.add("card", "col-sm-4", "col-md-3", "gy-3", "gx-3", "h-100");
+      /*   const col = document.createElement("div"); */
+      card.classList.add("card", "col-sm-4", "col-md-3", "gy-3", "gx-3");
       row.appendChild(card);
-      const col = document.ge;
-      card.style.maxWidth = "20%";
-
+      card.style = "height:35rem";
       const titolo = document.createElement("p");
-      titolo.innerText = book.title;
 
+      titolo.innerText = book.title;
+      const p3 = document.createElement("p");
+      p3.innerText = book.price;
       const img = document.createElement("img");
       img.src = book.img;
       card.appendChild(img);
+      console.log(book.img);
       card.appendChild(titolo);
+      card.appendChild(p3);
       img.style = " height:300px;  object-fit: cover";
+      card.appendChild(btn2);
       card.appendChild(btn1);
     });
   })
